081469118c42c62422ca2bd7e30cf6442ba50e27
Daisuke Sasaki <gerund25495641@gmail.com>
2021-05-06 05:53:46 +0900

    learned how to get Web API by using JavaScript

--- js/main.js
+++ js/main.js
@@ -96,32 +96,32 @@ window.console.log(1+2+3);
 
 //匿名関数 =関数がなくても動く
 
-const isTweetable   = function isTweetable(text){
-    return text.length <= 140;
-};
+// const isTweetable   = function isTweetable(text){
+//     return text.length <= 140;
+// };
 
-console.log(isTweetable("foo"));
+// console.log(isTweetable("foo"));
 
 
-function alertTweetable(text){
-    if (isTweetable(text)){
-       console.log("you can tweet!");
-    }
-};
+// function alertTweetable(text){
+//     if (isTweetable(text)){
+//        console.log("you can tweet!");
+//     }
+// };
 
-function unfollow(){
-    console.log("フォローを外しましたか");
-}
+// function unfollow(){
+//     console.log("フォローを外しましたか");
+// }
 
-function cancelTweet(){
-    console.log("ツイートをキャンセルしました");
-}
+// function cancelTweet(){
+//     console.log("ツイートをキャンセルしました");
+// }
 
-function confirmed(fn){
-    if(window.confirm("実行しますか")){
-        fn();
-    }
-}
+// function confirmed(fn){
+//     if(window.confirm("実行しますか")){
+//         fn();
+//     }
+// }
 
 // confirmed(unfollow);
 
@@ -146,3 +146,89 @@ console.log(obj.foo);
 //オブジェクトのメソッドが関数
 
 
+
+const foo  = {
+    alert: function (){},
+    alert(){},
+};
+
+foo.alert();
+
+
+// function 関数名(引数１、引数２){
+//     return 関数の返り値
+// }
+
+
+//キャメルケース = 単語の先頭を大文字にする  CamelCase
+
+
+//スネークケース = 単語の間をアンダーバーでつなぐ命名規則 hapitas_comic
+
+
+
+
+function cut(food){
+    const cutFood = food.slice();
+    return  cutFood;
+}
+
+// const cutCarrot = cut(carrot);
+// const cutPotato = cut(banana); 
+
+
+function throwAway(food){
+    delete food;
+}
+
+function isTweetable(text){ 
+    return text.length <= 140;
+}
+
+console.log(isTweetable("テストテスト"));
+
+function alertTweetable(text){
+    if(text.length <= 140){
+        alert('you can tweet!');
+    }
+}
+
+// alertTweetable('foo');
+
+
+//Web APIとは
+
+//バックエンドエンジニアがAPIを用意する
+//フロントエンドはAPIを使ってUIを構築する
+
+
+
+    //実際にAPIを叩く処理
+//    const res =  await fetch("https://jsonplaceholder.typicode.com/users");
+//    const users  = await res.json();
+//    console.log(users);
+
+// function callApi(){
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(function(res){
+//         return res.json();
+//     })
+//     .then(function(users){
+//         console.log(users);  
+//     })
+// }
+
+// callApi();
+
+
+function callapi(){
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+    xhr.responseType = "json";
+    xhr.send();
+    xhr.onload = function(){
+        console.log(xhr.response);
+    }
+}
+
+callapi();
